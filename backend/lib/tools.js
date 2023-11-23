78067f1c686b6003e6a782ef6b2b2ac0173c1594
Byeongchan Gwak <kbckbc@gmail.com>
2023-11-23 17:12:38 +0900

    add a custom log function

--- backend/lib/tools.js
+++ backend/lib/tools.js
@@ -51,5 +51,20 @@ module.exports = {
     else {
       return {ret:1};
     }
-  }
+  },
+
+  // This accepts rest parameter for an arbitrary amount of arguments
+  log: (...msg) => {
+    const STACK_FUNC_NAME = new RegExp(/\w+\.\w+:(\d+)/);
+    let err = new Error();
+    
+    Error.captureStackTrace(err);
+  
+    let stacks = err.stack.split('\n').slice(1);
+    // console.log(err.stack);
+  
+    // In Error trace, caller information is in the second array.
+    let callerInfo = STACK_FUNC_NAME.exec(stacks[1]);
+    console.log(`${callerInfo[0]}  ${msg}`);
+  },
 };
